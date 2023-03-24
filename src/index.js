3a2ea861c7ff4c0908785914fc9c308903267ddd
mohamedibrahim5776 <mohamedibrahim5776@gmail.com>
2023-03-24 10:26:10 +0200

    upload

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ let sky = colors[1]
 let blueViolet = colors[2]
 let green = colors[3]
 let colorCurrant = window.localStorage.getItem('activeColor')
+let [...option] = document.querySelectorAll('.option')
+let content = document.querySelector('.container')
+
 // to know which main color
 window.onclick = function activeColor () {
     return window.getComputedStyle(document.documentElement).getPropertyValue('--main-color')
@@ -25,17 +28,49 @@ icon.addEventListener('click', function () {
     this.classList.toggle('anime')
 })
 // on click the color set it as main color and add class active
-colors.map((el, index, arr) => {
-    if (el.style.backgroundColor == colorCurrant.trim()) {
-        el.classList.add('active')
-    }
-    el.onclick = function () {
-        document.documentElement.style.setProperty('--main-color', el.style.backgroundColor);
-        arr.forEach((el) => {
-            el.classList.remove('active')
+
+function activeOption(arr) {
+    arr.map((el, ind, arr) => {
+        if (el.style.backgroundColor == colorCurrant.trim()) {
+            el.classList.add('active')
+        }
+        el.addEventListener('click', function () {
+            console.log(el.classList.contains('option'))
+            if (el.classList.contains('option')) {
+                function change () {
+                    content.style.backgroundImage = `url(../img/${Math.ceil(Math.random() * 9)}.jpg)`
+                    if (option[1].classList.contains('active')== true) {
+                        clearInterval(changeImg)
+                    }
+                }
+                let changeImg = setInterval( change ,3000)
+            }
+            if (el.classList.contains('optionColor')) {
+                document.documentElement.style.setProperty('--main-color', el.style.backgroundColor);
+            }
             arr.forEach((el) => {
-                this.classList.add('active')
+                el.classList.remove('active')
+                arr.forEach((el) => {
+                    this.classList.add('active')
+                })
             })
         })
-    }
-})
+    })
+}
+activeOption(option)
+activeOption(colors)
+
+
+
+
+
+//     arr.forEach((el) => {
+//         el.classList.remove('active')
+//             arr.forEach((el) => {
+//                 this.classList.add('active')
+//             })
+//     })
+// }
+
+
+
